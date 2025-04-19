9233a3d53b99d4a3ad45558f751b8b3de55f2134
Sapnil Bhowmik <bhowmiksapnil66138@gmail.com>
2025-04-19 17:20:47 +0530

    add ticketAssign API

--- src/Controllers/ticket.controller.js
+++ src/Controllers/ticket.controller.js
@@ -1,39 +1,73 @@
+const createHTTPError = require("http-errors")
 
-const getAllTickets = (req,res,next) => {
-    try{
-        
+const {
+    ticketModel
+} = require("../Models/index.js")
+
+const getAllTickets = (req, res, next) => {
+    try {
+
     }
-    catch(err){
+    catch (err) {
         next(err)
     }
 }
 
 
-const getAllTickets_byStatus = (req,res,next) => {
-    try{
-        
+const getAllTickets_byStatus = (req, res, next) => {
+    try {
+
     }
-    catch(err){
+    catch (err) {
         next(err)
     }
 }
 
 
-const setTicketStatus = (req,res,next) => {
-    try{
+const setTicketStatus = (req, res, next) => {
+    try {
         console.log(req.query)
         console.log(req.body)
     }
-    catch(err){
+    catch (err) {
         next(err)
     }
 }
 
 
+const assignTicket = async (req, res, next) => {
+    try {
+        const { memberID } = req.params
+        const { ticketID } = req.body
+        if (!memberID) throw createHTTPError.BadRequest("Member ID is required")
+        if (!ticketID) throw createHTTPError.BadRequest("Ticket ID is required")
+
+        const {isMember} = req.LoggedIn_UserInfo
+        if(isMember) throw createHTTPError.Forbidden("Only Admin can assign tickets to team members")
+
+        const existingTicket = await ticketModel.findById(ticketID)
+        if(!existingTicket) throw createHTTPError.BadRequest("Ticket Not Found")
+        
+        let updatedTicket = await ticketModel.findByIdAndUpdate(ticketID , {
+            assignID: memberID,
+            assignType: "MEMBER"
+        } , {new: true})
+
+        res.json({
+            message: "Ticket assigned to team member successfully",
+            data: updatedTicket
+        })
+
+    }
+    catch (err) {
+        next(err)
+    }
+}
 
 
 module.exports = {
     getAllTickets,
     getAllTickets_byStatus,
-    setTicketStatus
+    setTicketStatus,
+    assignTicket
 }
--- src/Routes/ticket.route.js
+++ src/Routes/ticket.route.js
@@ -5,15 +5,18 @@ const ticketRouter = express.Router()
 const {
     getAllTickets,
     getAllTickets_byStatus,
-    setTicketStatus
+    setTicketStatus,
+    assignTicket
 } = require("../Controllers/ticket.controller.js")
 
+const {userAuth} = require("../Middlewares/auth.middleware.js")
 
-ticketRouter.get("/all" , getAllTickets)
-ticketRouter.get("/all/:status" , getAllTickets_byStatus)
+ticketRouter.get("/all" , userAuth , getAllTickets)
+ticketRouter.get("/all/:status" , userAuth , getAllTickets_byStatus)
 
 // Pass query params -> resolved / unresolved
-ticketRouter.post("/" , setTicketStatus)
+ticketRouter.post("/" , userAuth , setTicketStatus)
+ticketRouter.post("/assign/:memberID" , userAuth , assignTicket)
 
 
 module.exports = {
